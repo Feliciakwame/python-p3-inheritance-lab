d244fb977a05c8fcb5b9e5d3e90839a14fcdd039
Felicia Kwame <kwamefelicia2005@gmail.com>
2025-01-31 15:51:19 +0300

    final commit

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+    knowledge=[]
+    def __init__(self,first_name,last_name):
+        
+        super().__init__(first_name,last_name,)
     
-    def learn(self):
-        pass
+    def learn(self,new_knowledge):
+        if isinstance(new_knowledge,str):
+            self.knowledge.append(new_knowledge)
+        else:
+            return "Please enter a string"
+        
